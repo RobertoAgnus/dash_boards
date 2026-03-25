908b9f1b869cb884655861d169a3501b355fa8c3
Roberto <roberto@agnuspromotora1.com.br>
2026-03-25 09:43:28 -0300

    ajustes na conexão

--- querys/connect.py
+++ querys/connect.py
@@ -1,16 +1,17 @@
 import mysql.connector
 import psycopg2
 import streamlit as st
+from sqlalchemy import create_engine
 
 class Conexao:
     def __init__(self):
-        self.config_mysql_aws = {
-            'user'    : st.secrets["database"]["USER_MYSQL_AWS"],
-            'password': st.secrets["database"]["PASS_MYSQL_AWS"],
-            'host'    : st.secrets["database"]["HOST_MYSQL_AWS"],
-            'database': st.secrets["database"]["DATABASE_MYSQL_AWS"],
-            'port'    : st.secrets["database"]["PORT_MYSQL_AWS"]
-        }
+        # self.config_mysql_aws = {
+        #     'user'    : st.secrets["database"]["USER_MYSQL_AWS"],
+        #     'password': st.secrets["database"]["PASS_MYSQL_AWS"],
+        #     'host'    : st.secrets["database"]["HOST_MYSQL_AWS"],
+        #     'database': st.secrets["database"]["DATABASE_MYSQL_AWS"],
+        #     'port'    : st.secrets["database"]["PORT_MYSQL_AWS"]
+        # }
         self.config_postgres_aws = {
             'user'    : st.secrets["database"]["USER_POSTGRES_AWS"],
             'password': st.secrets["database"]["PASS_POSTGRES_AWS"],
@@ -29,13 +30,27 @@ class Conexao:
         self.conn_postgres_aws = None
         self.conn_postgres     = None
 
-    def conectar_mysql_aws(self):
-        try:
-            self.conn_mysql_aws = mysql.connector.connect(**self.config_mysql_aws)
-            print("Conexão MySQL AWS bem-sucedida!")
-        except mysql.connector.Error as err:
-            print(f"Erro ao conectar ao MySQL AWS: {err}")
-            self.conn_mysql_aws = None
+        config_postgres_aws = self.config_postgres_aws
+
+        self.engine_aws = create_engine(
+            f"postgresql+psycopg2://{config_postgres_aws['user']}:{config_postgres_aws['password']}@"
+            f"{config_postgres_aws['host']}:{config_postgres_aws['port']}/{config_postgres_aws['dbname']}"
+        )
+
+        config_postgres = self.config_postgres
+
+        self.engine = create_engine(
+            f"postgresql+psycopg2://{config_postgres['user']}:{config_postgres['password']}@"
+            f"{config_postgres['host']}:{config_postgres['port']}/{config_postgres['dbname']}"
+        )
+
+    # def conectar_mysql_aws(self):
+    #     try:
+    #         self.conn_mysql_aws = mysql.connector.connect(**self.config_mysql_aws)
+    #         print("Conexão MySQL AWS bem-sucedida!")
+    #     except mysql.connector.Error as err:
+    #         print(f"Erro ao conectar ao MySQL AWS: {err}")
+    #         self.conn_mysql_aws = None
 
     def conectar_postgres_aws(self):
         try:
@@ -54,10 +69,10 @@ class Conexao:
             print(f"Erro ao conectar ao PostgreSQL: {err}")
             self.conn_postgres = None
 
-    def desconectar_mysql_aws(self):
-        if self.conn_mysql_aws:
-            self.conn_mysql_aws.close()
-            print("Conexão ao MySQL AWS encerrada.")
+    # def desconectar_mysql_aws(self):
+    #     if self.conn_mysql_aws:
+    #         self.conn_mysql_aws.close()
+    #         print("Conexão ao MySQL AWS encerrada.")
 
     def desconectar_postgres_aws(self):
         if self.conn_postgres_aws:
@@ -70,8 +85,8 @@ class Conexao:
             self.conectar_postgres = None
             print("Conexão ao PostgreSQL encerrada.")
 
-    def obter_conexao_mysql_aws(self):
-        return self.conn_mysql_aws
+    # def obter_conexao_mysql_aws(self):
+    #     return self.conn_mysql_aws
     
     def obter_conexao_postgres_aws(self):
         return self.conn_postgres_aws
